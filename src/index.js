bf097776df3e7e23566e15778f82b86f95737d4a
Robert Kempton <r@snow87.com>
2019-10-23 23:17:22 -0600

    add exponential backoff for restarts

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ const cluster = require('cluster')
 
 /**
  * Startup function for the spliffy server
+ * Spliffy will start up in a forked process so that if any uncaught exceptions are thrown a new process can be initialized and the server restarted.
+ * Startup will exponentially back off if the server crashes within 1 second of starting up
  * @param config See https://github.com/narcolepticsnowman/spliffy#config
  * @returns {Promise<void>} an empty promise...
  */
+let consecutiveFailures = 0
+let lastStart = new Date().getTime()
 const spliffy = (config)=>{
     if(cluster.isMaster){
         cluster.fork()
         cluster.on('exit', (worker)=>{
             log.info('Server crashed, restarting in 100ms')
-            setTimeout(()=>cluster.fork(), 100)
+            const now = new Date().getTime()
+            if(now - lastStart <= 1000) {
+                consecutiveFailures++
+            } else {
+                consecutiveFailures = 0
+            }
+            setTimeout(()=>{
+                cluster.fork()
+                lastStart = new Date().getTime()
+            }, Math.pow(2,consecutiveFailures) * 100 )
         })
     } else {
         start(config)
