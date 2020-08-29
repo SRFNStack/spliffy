aa2ed14519b6f71efeb6cbbe41b763e55b6ff3db
Robert Kempton <r@snow87.com>
2020-08-29 11:58:54 -0600

    update docs to reflect that responses can be thrown too

--- docs/home.js
+++ docs/home.js
@@ -1,25 +1,25 @@
-import { a, div, h2, h3, li, p, pre, strong, ul, code } from './fnelements.js'
+import { a, b, div, h2, h3, li, p, pre, strong, ul } from './fnelements.js'
 import prismCode from './prismCode.js'
 import { fnlink } from './fntags.js'
 
 export default div( { class: 'flex-center', style: 'flex-direction: column; font-size: 16px;' },
                     h2( { id: 'getting-started' }, 'Getting started' ),
                     p( 'Create a directory for your app' ),
-                    p( prismCode('mkdir -p ~/app/www' )),
+                    p( prismCode( 'mkdir -p ~/app/www' ) ),
                     p( 'Install spliffy' ),
-                    p( prismCode('cd ~/app && npm install spliffy' )),
+                    p( prismCode( 'cd ~/app && npm install spliffy' ) ),
                     p( 'Create a handler for the desired route name (a regular js file with the suffix .rt.js) ' ),
-                    p( prismCode('vi ~/app/www/spliffy.rt.js' )),
+                    p( prismCode( 'vi ~/app/www/spliffy.rt.js' ) ),
                     prismCode( 'module.exports = {\n    GET:() => ({hello: "spliffy"})\n}' ),
                     p( 'Create the start script, ', prismCode( 'vi ~/app/serve.js' ) ),
                     pre( prismCode( 'require(\'spliffy\')({routeDir: __dirname+ \'/www\'})' ) ),
                     p( 'The spliffy.rt.js file in ~/app/www creates the route',
                        prismCode( '/spliffy' ) ),
-                    p( 'See ',fnlink( { to: '/config' }, 'Config' ),' for a complete set of options.' ),
+                    p( 'See ', fnlink( { to: '/config' }, 'Config' ), ' for a complete set of options.' ),
                     p( 'routeDir is the only required property and should be an absolute path.' ),
                     p( '10420 is the default port for http, and can be changed by setting the port in the config' ),
                     p( 'start the server', prismCode( 'node ~/app/serve.js' ) ),
-                    p( 'Go to ',prismCode('localhost:10420/spliffy' )),
+                    p( 'Go to ', prismCode( 'localhost:10420/spliffy' ) ),
 
 
                     h3( { id: '-examples-https-github-com-narcolepticsnowman-spliffy-tree-master-example-' },
@@ -33,7 +33,7 @@ export default div( { class: 'flex-center', style: 'flex-direction: column; font
                     ) ),
                     p( 'The exported properties are all caps request methods, any request method is allowed.' ),
                     p( 'Files named index.rt.js can be created to handle the route of the name of the folder just like in apache.' ),
-                    h3({id: 'handler-arguments'}, 'Handler arguments:' ),
+                    h3( { id: 'handler-arguments' }, 'Handler arguments:' ),
                     ul(
                         li( strong( 'url' ), ': An object containing path and parameter information about the url',
                             ul(
@@ -48,7 +48,9 @@ export default div( { class: 'flex-center', style: 'flex-direction: column; font
                         li( strong( 'headers' ), ': The request headers' ),
                         li( strong( 'req' ), ': The node http.IncomingMessage with the additional property \'cookies\' that contains the cookies as parsed by the cookie library' ),
                         li( strong( 'res' ), ': The un-adulterated node http.ServerResponse' ),
-                        li( strong( 'setCookie' ), ': A helper function for setting cookies on the response. Arguments are passed verbatim to ', a({href:'https://www.npmjs.com/package/cookie#cookieserializename-value-options'}, 'cookie.serialize') ),
+                        li( strong( 'setCookie' ),
+                            ': A helper function for setting cookies on the response. Arguments are passed verbatim to ',
+                            a( { href: 'https://www.npmjs.com/package/cookie#cookieserializename-value-options' }, 'cookie.serialize' ) )
                     ),
 
 
@@ -56,7 +58,11 @@ export default div( { class: 'flex-center', style: 'flex-direction: column; font
                     p( 'The handler can return any kind of data and it will be serialized automatically if there is a known serializer for the specified content-type. The default, application/json, is already set.' ),
                     p( 'If the returned value is Falsey, we will assume everything is fine and return a 200 OK.' ),
                     p( 'You can return a promise that resolves to the response value, and the server will respond with the resolved value.' ),
-                    p( 'If you need to set the statusCode, headers, etc, you must return an object with a body property for the body and optionally one or more of the following properties' ),
+                    p( 'If you need to set the statusCode, headers, etc, you must either ',
+                       b( 'return' ),
+                       'or ',
+                       b( 'throw' ),
+                       ' an object with a body property for the body and optionally one or more of the following properties' ),
                     prismCode( `{
     headers: {
         "cache-control": 'no-cache'
@@ -78,5 +84,5 @@ export default div( { class: 'flex-center', style: 'flex-direction: column; font
                         li( 'Server side rendering (aka templating/mvc' ),
                         li( 'proxy address trust and x-forwarded-for using proxy-addr' ),
                         li( 'Production mode that creates a manifest containing all of the known routes, any files not in the manifest won\'t be loaded. This will prevent routes being added at runtime.' )
-                    ),
                     )
+)
